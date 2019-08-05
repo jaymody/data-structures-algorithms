e2f61237d5ea79d91463d90e99488f2dfce43b1c
Jay Mody <jaykmody@gmail.com>
2019-08-05 17:30:24 -0400

    more additiions to binary tree algorithim

--- trees/binary_tree.c
+++ trees/binary_tree.c
@@ -12,7 +12,7 @@ struct Node {
 
 // binary tree structure
 struct BinaryTree {
-    struct Node* head;
+    struct Node* root;
 };
 
 
@@ -27,20 +27,63 @@ struct Node* CreateNode(int val) {
 
 // allocate new binary tree
 struct BinaryTree* CreateBinaryTree() {
-    struct BinaryTree* tree = malloc(sizeof(BinaryTree));
-    tree->head = NULL;
+    struct BinaryTree* tree = malloc(sizeof(struct BinaryTree));
+    tree->root = NULL;
+    return tree;
 }
 
-// add value to binary tree
-void AddNode(struct Node* node, int val) {
-    if (node == NULL)
-        node = CreateNode(val);
-    else if (val <= node->val)
-        AddNode(node->left);
-    else if (val > node->val)
-        AddNode(node->right);
+// insert value to binary tree
+void _InsertNode(struct Node** node, int val) {
+    if (*node == NULL)
+        *node = CreateNode(val);
+    else if (val <= (*node)->val)
+        _InsertNode(&(*node)->left, val);
+    else
+        _InsertNode(&(*node)->right, val);
 }
-void AddNode(struct BinaryTree* tree, int val) {
-    AddNode(tree->head, val);
+void InsertNode(struct BinaryTree* tree, int val) {
+    _InsertNode(&tree->root, val);
 }
 
+// print tree
+// WARNING: only works for smaller trees with small values
+void _PrintTree(struct Node* root, int space) {
+    if (root == NULL)
+        return;
+
+    space += 10;
+
+    _PrintTree(root->right, space);
+
+    printf("\n");
+    for (int i = 10; i < space; i++)
+        printf(" ");
+    printf("%d", root->val);
+    
+    _PrintTree(root->left, space);
+}
+void PrintTree(struct BinaryTree* tree) {
+    _PrintTree(tree->root, 0);
+    printf("\n");
+}
+
+
+// main
+int main() {
+    struct BinaryTree* mytree = CreateBinaryTree();
+    InsertNode(mytree, 10);
+    InsertNode(mytree, 5);
+    InsertNode(mytree, 4);
+    InsertNode(mytree, 8);
+    InsertNode(mytree, 7);
+    InsertNode(mytree, 9);
+    InsertNode(mytree, 3);
+    InsertNode(mytree, 11);
+    InsertNode(mytree, 20);
+    InsertNode(mytree, 15);
+    InsertNode(mytree, 16);
+    InsertNode(mytree, 17);
+    InsertNode(mytree, 13);
+
+    PrintTree(mytree);
+}
