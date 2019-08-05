88c9596d712c6a77806beeacd3c42e16d6ac27a1
Jay Mody <jaykmody@gmail.com>
2019-08-05 18:19:46 -0400

    binary search tree pre,in,post order visit implemnetations

--- trees/binary_tree.c
+++ trees/binary_tree.c
@@ -1,4 +1,4 @@
-// binary tree implementation
+// binary search tree implementation
 // includes
 #include <stdio.h>
 #include <stdlib.h>
@@ -45,6 +45,65 @@ void InsertNode(struct BinaryTree* tree, int val) {
     _InsertNode(&tree->root, val);
 }
 
+
+
+
+
+// pre order visit
+void _PreOrderVisit(struct Node* root, void (*fn)(struct Node*)) {
+    if (root == NULL)
+        return;
+    else {
+        (*fn)(root);
+        _PreOrderVisit(root->left, fn);
+        _PreOrderVisit(root->right, fn);
+    }
+}
+void PreOrderVisit(struct BinaryTree* tree, void (*fn)(struct Node*)) {
+    _PreOrderVisit(tree->root, fn);
+} 
+
+// in order visit
+void _InOrderVisit(struct Node* root, void (*fn)(struct Node*)) {
+    if (root == NULL)
+        return;
+    else {
+        _InOrderVisit(root->left, fn);
+        (*fn)(root);
+        _InOrderVisit(root->right, fn);
+    }
+}
+void InOrderVisit(struct BinaryTree* tree, void (*fn)(struct Node*)) {
+    _InOrderVisit(tree->root, fn);
+} 
+
+// postvisit
+void _PostOrderVisit(struct Node* root, void (*fn)(struct Node*)) {
+    if (root == NULL)
+        return;
+    else {
+        _PostOrderVisit(root->left, fn);
+        _PostOrderVisit(root->right, fn);
+        (*fn)(root);
+    }
+}
+void PostOrderVisit(struct BinaryTree* tree, void (*fn)(struct Node*)) {
+    _PostOrderVisit(tree->root, fn);
+}
+
+// print value at node
+void PrintNode(struct Node* node) {
+    printf("%d, ", node->val);
+}
+
+
+
+
+// clear tree
+void ClearTree(struct BinaryTree* tree) {
+
+}
+
 // print tree
 // WARNING: only works for smaller trees with small values
 void _PrintTree(struct Node* root, int space) {
@@ -73,17 +132,27 @@ int main() {
     struct BinaryTree* mytree = CreateBinaryTree();
     InsertNode(mytree, 10);
     InsertNode(mytree, 5);
-    InsertNode(mytree, 4);
+    InsertNode(mytree, 3);
     InsertNode(mytree, 8);
     InsertNode(mytree, 7);
     InsertNode(mytree, 9);
-    InsertNode(mytree, 3);
-    InsertNode(mytree, 11);
-    InsertNode(mytree, 20);
+    InsertNode(mytree, 2);
+    InsertNode(mytree, 4);
     InsertNode(mytree, 15);
-    InsertNode(mytree, 16);
-    InsertNode(mytree, 17);
     InsertNode(mytree, 13);
+    InsertNode(mytree, 18);
+    InsertNode(mytree, 17);
+    InsertNode(mytree, 19);
+    InsertNode(mytree, 12);
+    InsertNode(mytree, 14);
 
     PrintTree(mytree);
+
+    printf("PreOrder:\t");
+    PreOrderVisit(mytree, PrintNode);
+    printf("\nInOrder:\t");
+    InOrderVisit(mytree, PrintNode);
+    printf("\nPostOrder:\t");
+    PostOrderVisit(mytree, PrintNode);
+    printf("\n");
 }
