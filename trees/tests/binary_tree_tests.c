482ac33502d2f1c2be304a5bc084d334be700f0b
Jay Mody <jaykmody@gmail.com>
2019-08-08 16:29:56 -0400

    better unit testing format as well as more test cases

--- trees/tests/binary_tree_tests.c
+++ trees/tests/binary_tree_tests.c
@@ -1,37 +1,115 @@
 ////// Includes //////
 #include "../binary_tree.c"
 
-void test1() {
+////// Tests //////
+void test0() {
+    /* null test */
+    // Create
     struct BinaryTree* mytree = CreateBinaryTree();
-    InsertNode(mytree, 10);
-    InsertNode(mytree, 5);
-    InsertNode(mytree, 3);
-    InsertNode(mytree, 8);
-    InsertNode(mytree, 7);
-    InsertNode(mytree, 9);
-    InsertNode(mytree, 2);
-    InsertNode(mytree, 4);
-    InsertNode(mytree, 15);
-    InsertNode(mytree, 13);
-    InsertNode(mytree, 18);
-    InsertNode(mytree, 17);
-    InsertNode(mytree, 19);
-    InsertNode(mytree, 12);
-    InsertNode(mytree, 14);
+    printf("Is Empty after init:\t%s\n", IsEmpty(mytree) ? "true" : "false");
 
+    // Insert
+    printf("Inserting:\t%s\n", "{}");
+    printf("Is Empty after Insert:\t%s\n", IsEmpty(mytree) ? "true" : "false");
+
+    // Print
+    printf("Print Tree:\n");
     PrintTree(mytree);
+    printf("\n");
 
+    // Visits
     printf("PreOrder:\t");
     PreOrderVisit(mytree, PrintNode);
+
     printf("\nInOrder:\t");
     InOrderVisit(mytree, PrintNode);
+
     printf("\nPostOrder:\t");
     PostOrderVisit(mytree, PrintNode);
+
+    // Clear
+    ClearTree(mytree);
+    printf("\nIs Empty after clear:\t%s\n", IsEmpty(mytree) ? "true" : "false");
+
+}
+void test1() {
+    /* perfect binary tree test */
+    // Create
+    struct BinaryTree* mytree = CreateBinaryTree();
+    printf("Is Empty after init:\t%s\n", IsEmpty(mytree) ? "true" : "false");
+
+    // Insert
+    int vals[15] = {10,5,3,8,7,9,2,4,15,13,18,17,19,12,14};
+    printf("Inserting:\t%s\n", "{10,5,3,8,7,9,2,4,15,13,18,17,19,12,14}");
+    for (int i = 0; i < sizeof(vals)/sizeof(vals[0]); i++)
+        InsertNode(mytree, vals[i]);
+    printf("Is Empty after insert:\t%s\n", IsEmpty(mytree) ? "true" : "false");
+
+    // Print Tree
+    printf("Print Tree:\n");
+    PrintTree(mytree);
     printf("\n");
+
+    // Visits
+    printf("PreOrder:\t"); 
+    PreOrderVisit(mytree, PrintNode);
+
+    printf("\nInOrder:\t"); 
+    InOrderVisit(mytree, PrintNode);
+
+    printf("\nPostOrder:\t"); 
+    PostOrderVisit(mytree, PrintNode);
+
+    // Clear
+    ClearTree(mytree);
+    printf("\nIs Empty after clear:\t%s\n", IsEmpty(mytree) ? "true" : "false");
+}
+void test2() {
+    /* perfect binary tree test */
+    // Create
+    struct BinaryTree* mytree = CreateBinaryTree();
+    printf("Is Empty after init:\t%s\n", IsEmpty(mytree) ? "true" : "false");
+
+    // Insert
+    int vals[10] = {-10,-1,-3,-8,-4,-29,-2,-14,-15,-13};
+    printf("Inserting:\t%s\n", "{-10,-1,-3,-8,-4,-29,-2,-14,-15,-13}");
+    for (int i = 0; i < sizeof(vals)/sizeof(vals[0]); i++)
+        InsertNode(mytree, vals[i]);
+    printf("Is Empty after insert:\t%s\n", IsEmpty(mytree) ? "true" : "false");
+
+    // Print Tree
+    printf("Print Tree:\n");
+    PrintTree(mytree);
+    printf("\n");
+
+    // Visits
+    printf("PreOrder:\t"); 
+    PreOrderVisit(mytree, PrintNode);
+
+    printf("\nInOrder:\t"); 
+    InOrderVisit(mytree, PrintNode);
+
+    printf("\nPostOrder:\t"); 
+    PostOrderVisit(mytree, PrintNode);
+
+    // Clear
+    ClearTree(mytree);
+    printf("\nIs Empty after clear:\t%s\n", IsEmpty(mytree) ? "true" : "false");
+}
+
+////// Run Test //////
+void run_test(char name[], void (*test)()) {
+    printf("############################################################\n");
+    printf("##########\t%s\n", name);
+    printf("############################################################\n");
+    (*test)();
+    printf("\n\n\n");
 }
 
 ////// Main //////
 int main() {
-    test1();
+    run_test("NULL TEST", test0);
+    run_test("PERFECT BINARY TREE TEST", test1);
+    run_test("NEGATIVE NUMBERS TEST", test2);
     return 1;
 }
