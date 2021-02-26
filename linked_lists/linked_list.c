80f5a6d6a999d4f06d8c5f68f0843a01ac29d8eb
jaymody <jaykmody@gmail.com>
2021-02-26 08:20:56 -0500

    huh

--- linked_lists/linked_list.c
+++ linked_lists/linked_list.c
@@ -1,125 +1,93 @@
-/* singly linked list implementation */
-
-////// Includes //////
 #include <stdio.h>
 #include <stdlib.h>
 
-
-////// Structs ///////
-struct Node {
+/* structs */
+typedef struct Node {
     int val;
-    struct Node* next;
-};
+    struct Node *next;
+} Node;
 
-struct LinkedList {
+typedef struct LinkedList {
     int size;
-    struct Node* head;
-};
+    Node *head;
+} LinkedList;
 
-
-////// Create Structs ///////
-struct Node* CreateNode(int val) {
-    struct Node* node = malloc(sizeof(struct Node));
+/* create structs */
+Node *CreateNode(int val) {
+    Node *node = malloc(sizeof(Node));
     node->next = NULL;
     node->val = val;
     return node;
 }
 
-struct LinkedList* CreateList() {
-    struct LinkedList* list = malloc(sizeof(struct LinkedList));
+LinkedList *CreateLinkedList() {
+    LinkedList *list = malloc(sizeof(LinkedList));
     list->head = NULL;
     list->size = 0;
     return list;
 }
 
+/* functions */
+void InsertNode(LinkedList *list, Node *node) {
+    if (!list->head) {
+        list->head = node;
+    } else if (list->head->val > node->val) {
+        node->next = list->head;
+        list->head = node;
+    } else {
+        Node *head = list->head;
+        while (head->next && head->next->val < node->val)
+            head = head->next;
 
-////// Linked List Functions //////
-void InsertHead(struct LinkedList* list, int val) {
-    struct Node* node = CreateNode(val);
-    node->next = list->head;
-    list->head = node;
+        node->next = head->next;
+        head->next = node;
+    }
     list->size++;
 }
 
-void DeleteHead(struct LinkedList* list) {
-    if (list->head) {
-        struct Node* temp = list->head;
-        list->head = list->head->next;
-        free(temp);
-        list->size--;
+void ClearList(LinkedList *list) {
+    Node *temp;
+    while (list->head) {
+        temp = list->head->next;
+        free(list->head);
+        list->head = temp;
     }
-}
-
-void InsertTail(struct LinkedList* list, int val) {
-    struct Node* new_node = CreateNode(val);
-
-    if (!list->head)
-        list->head = new_node;
-    else {
-        struct Node* node = list->head;
-        while (node->next != NULL) {
-            node = node->next;
-        }
-        node->next = new_node;
-    }
-
-    list->size++;
-}
-
-void DeleteTail(struct LinkedList* list) {
-    if (list->head) {
-        struct Node* node = list->head;
-        struct Node* prev = NULL;
-
-        do {
-            prev = node;
-            node = node->next;
-        } while(node->next != NULL);
-
-        free(node);
-        prev->next = NULL;
-        list->size--;
-    }
-}
-
-void ReverseList(struct LinkedList* list) {
-    struct Node* current = list->head;
-    struct Node* rev = NULL;
-    struct Node* temp = NULL;
-
-    while (current != NULL) {
-        temp = current;
-        current = current->next;
-        temp->next = rev;
-        rev = temp;
-    }
-
-    list->head = rev;
-}
-
-void PrintList(struct LinkedList* list) {
-    struct Node* node = list->head;
-
-    while (node != NULL) {
-        printf("%d, ", node->val);
-        node = node->next;
-    }
-    printf("\n");
-}
-
-void ClearList(struct LinkedList* list) {
-    struct Node* node = list->head;
-
-    while (node != NULL) {
-        list->head = node->next;
-        free(node);
-        node = list->head;
-    }
-
-    free(list->head);
     list->size = 0;
 }
 
-int Length(struct LinkedList* list) {
-    return list->size;
+void PrintList(LinkedList *list) {
+    Node *head = list->head;
+    while (head) {
+        printf("%d -> ", head->val);
+        head = head->next;
+    }
+    printf("null\n");
+}
+
+/* tests */
+int main() {
+    LinkedList *list = CreateLinkedList();
+
+    /* test 1 */
+    InsertNode(list, CreateNode(1));
+    InsertNode(list, CreateNode(2));
+    InsertNode(list, CreateNode(3));
+    PrintList(list);
+    printf("size: %d\n\n", list->size);
+    ClearList(list);
+
+    /* test 2 */
+    InsertNode(list, CreateNode(1));
+    InsertNode(list, CreateNode(2));
+    InsertNode(list, CreateNode(3));
+    PrintList(list);
+    printf("size: %d\n\n", list->size);
+    ClearList(list);
+
+    /* test 3 */
+    for (int i = 0; i < 10; i++)
+        InsertNode(list, CreateNode(rand() % 100));
+    PrintList(list);
+    printf("size: %d\n\n", list->size);
+    ClearList(list);
 }
